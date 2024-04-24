c7c4ed6b2cb481905d501bdee71b8d213b423624
Natanael Sosa <natanaelsosa@gmail.com>
2024-04-24 10:37:44 -0400

    update auth and home

--- blogFlask/home.py
+++ blogFlask/home.py
@@ -1,10 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 bp= Blueprint('home',__name__)
 
 @bp.route('/')
 def index():
-    return "pagina de inicio"
+    return render_template("index.html")
 
 @bp.route("/blog")
 def blog():
