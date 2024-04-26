8aae2355eee1e3c2dc4cd7466b65c855fef718dd
Natanael Sosa <natanaelsosa@gmail.com>
2024-04-25 22:29:07 -0400

    add config datebase

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
+
+SQLITE = 'sqlite:///blogflask.db'
+POSTGREESQL = 'postgresql+psycopg2://postgres:123456@localhost:5432/blogpost_db'
+POSTGREESQL1 = 'postgresql://scott:tiger@localhost/project'
 class Config:
     DEBUG = True
     SECRET_KEY = 'dev'
+    SQLALCHEMY_DATABASE_URI = POSTGREESQL
