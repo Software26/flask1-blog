e5dc4dc5f69c9498258e4a9d370b8b839426c566
Natanael Sosa <natanaelsosa@gmail.com>
2024-04-28 16:37:26 -0400

    update post.py

--- blogFlask/post.py
+++ blogFlask/post.py
@@ -1,17 +1,25 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, redirect ,g, url_for
+
+from .auth import login_required
+from .models import Post
+from blogFlask import db
+
 
 bp= Blueprint('post', __name__, url_prefix = '/post')
 
-
 @bp.route('/posts')
+@login_required
 def posts():
-    return render_template("admin/posts.html")
+    posts = Post.query.all()#list posts
+    return render_template("admin/posts.html",posts = posts)
 
 
-@bp.route('/create')
+@bp.route('/create', methods=("GET","POST"))
+@login_required
 def create():
     return render_template("admin/create.html")
 
 @bp.route('/update')
+@login_required
 def update():
     return render_template("admin/update.html")
