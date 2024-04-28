3eed4d9cdae242df680c004db626711e6cf86468
Natanael Sosa <natanaelsosa@gmail.com>
2024-04-28 19:13:00 -0400

    function delete and edit

--- blogFlask/post.py
+++ blogFlask/post.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, flash, redirect ,g, url_for
+from flask import Blueprint, render_template, flash, redirect ,g, url_for, request
 
 from .auth import login_required
 from .models import Post
@@ -17,9 +17,57 @@ def posts():
 @bp.route('/create', methods=("GET","POST"))
 @login_required
 def create():
+    if request.method == "POST":
+        url = request.form.get('url')
+        url = url.replace(' ','-')
+        title = request.form.get('title')
+        info = request.form.get('info')
+        content = request.form.get('ckeditor')
+        
+        
+        post = Post(g.user.id, url, title, info, content)
+        
+        error = None
+        
+        #comprarando url de post con los existentes
+        post_url = Post.query.filter_by(url = url).first()
+        if post_url == None:
+            db.session.add(post)
+            db.session.commit()
+            flash(f"El blog {post.title} se registro correctamente")
+            return redirect(url_for('post.posts'))
+        else:
+           error = f"El url '{url}' ya esta registrado"
+        flash(error)
     return render_template("admin/create.html")
 
-@bp.route('/update')
+#Editar 
+
+# button delete
+
+
+@bp.route('/update/<int:id>', methods = ('GET', 'POST'))
 @login_required
-def update():
-    return render_template("admin/update.html")
+def update(id):
+    post = Post.query.get_or_404(id)
+    
+    if request.method == 'POST':
+        post.title = request.form.get('title')
+        post.info = request.form.get('info')
+        post.content = request.form['ckeditor']
+        db.session.commit()
+        
+        flash(f'El blog {post.title} se actualizo correctamente')   
+        return redirect(url_for('post.posts'))
+        
+    return render_template("admin/update.html", post = post)
+
+# Delete register
+@bp.route('/delete/<int:id>')
+@login_required
+def delete(id):
+    post = Post.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
+    flash(f'¡La blog {post.title} se a eliminado correctamente')
+    return redirect(url_for('post.posts'))
